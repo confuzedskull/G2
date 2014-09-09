f26d761e1b067705a47d101fcb8643253c72e7a3
James Nakano <jfunkymonkey007@gmail.com>
2014-09-09 13:56:17 -0700

    code is now under GPLv3
    to address the copyrights of my code, I applied the GNU Public License
    to the project

--- include/projectile.h
+++ include/projectile.h
@@ -1,3 +1,19 @@
+/*  This file is a part of 2DWorld - The Generic 2D Game Engine
+    Copyright (C) 2014  James Nakano
+
+    2DWorld is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    2DWorld is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with the rest of 2DWorld.  If not, see <http://www.gnu.org/licenses/>.*/
+
 #ifndef PROJECTILE_H
 #define PROJECTILE_H
 #include "physics_object.h"
